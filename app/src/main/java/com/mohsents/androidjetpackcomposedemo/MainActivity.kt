871e19b0f5964498abce24f8035e5b78fd2592f8
Mohsents <mohsenteimury2000@gmail.com>
2021-12-19 19:32:30 +0330

    Apply new look for greetings cards.

--- app/src/main/java/com/mohsents/androidjetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/mohsents/androidjetpackcomposedemo/MainActivity.kt
@@ -2,15 +2,18 @@ package com.mohsents.androidjetpackcomposedemo
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,12 +21,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mohsents.androidjetpackcomposedemo.ui.theme.AndroidJetpackComposeDemoTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     * and can be edited independently.
     */
     @Composable
-    fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    private fun MyApp() {
         /*
         * The remember() function works only as long as the composable is kept in the Composition.
         * The source of truth belongs to whoever creates and controls that state.
@@ -72,53 +76,75 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     private fun Greeting(name: String) {
+        Card(
+            backgroundColor = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+            CardContent(name = name)
+        }
+    }
+
+    @Composable
+    private fun CardContent(name: String) {
         // Preserve state across recompositions, remember the mutable state using remember().
         // remember() is used to guard against recomposition, so the state is not reset.
         // Each Greeting maintains its own expanded state, because they belong to different UI elements.
         var expanded by rememberSaveable { mutableStateOf(false) }
-        // Add extra padding with animation to the column based on expanded state.
-        val extraPadding by animateDpAsState(
-            if (expanded) 48.dp else 0.dp,
-            animationSpec = spring(
-                dampingRatio = Spring.DampingRatioLowBouncy,
-                stiffness = Spring.StiffnessMedium
-            )
-        )
-        // Using Surface() to change the background of elements.
-        Surface(
-            color = MaterialTheme.colors.primary,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        // Each child inside of a Row() will be placed horizontally.
+        // To add multiple modifiers to an element, you simply chain them.
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
-            // Each child inside of a Row() will be placed horizontally.
-            // To add multiple modifiers to an element, you simply chain them.
-            Row(modifier = Modifier.padding(24.dp)) {
-                Column(
-                    Modifier
-                        .weight(1f)
-                        // Ensure that padding greater than zero, otherwise app crash.
-                        .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-                ) {
-                    // Modifiers tell a UI element how to lay out, display, or behave within its parent layout.
-                    Text(text = "Hello, ")
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(12.dp)
+            ) {
+                // Modifiers tell a UI element how to lay out, display, or behave within its parent layout.
+                Text(text = "Hello, ")
+                Text(
+                    text = name,
+                    // Modify a predefined style by using the copy() function.
+                    // This way if you need to change the font family or any other attribute of h4,
+                    // you don't have to worry about the small deviations.
+                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                )
+                if (expanded) {
                     Text(
-                        text = name,
-                        // Modify a predefined style by using the copy() function.
-                        // This way if you need to change the font family or any other attribute of h4,
-                        // you don't have to worry about the small deviations.
-                        style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
                     )
                 }
-                OutlinedButton(
-                    onClick = { expanded = !expanded }
-                ) {
-                    Text(text = if (expanded) "Show less" else "Show more")
-                }
+            }
+            IconButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Icon(
+                    imageVector = if (expanded) {
+                        Icons.Filled.ExpandLess
+                    } else {
+                        Icons.Filled.ExpandMore
+                    },
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
         }
     }
 
     @Composable
-    fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    private fun OnboardingScreen(onContinueClicked: () -> Unit) {
+        // Use surface() to change the background
         Surface {
             Column(
                 modifier = Modifier.fillMaxSize(),
@@ -138,7 +164,7 @@ class MainActivity : AppCompatActivity() {
 
     @Preview(showBackground = true)
     @Composable
-    fun OnboardingPreview() {
+    private fun OnboardingPreview() {
         AndroidJetpackComposeDemoTheme {
             OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
         }
