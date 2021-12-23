2bb525b15276ccf71fadd56860f093edb16badc8
Mohsents <mohsenteimury2000@gmail.com>
2021-12-23 16:51:24 +0330

    Add lazyColumn list

--- app/src/main/java/com/mohsents/androidjetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/mohsents/androidjetpackcomposedemo/MainActivity.kt
@@ -3,12 +3,12 @@ package com.mohsents.androidjetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -19,20 +19,77 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.mohsents.androidjetpackcomposedemo.ui.theme.AndroidJetpackComposeDemoTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            AppLayout()
+            AndroidJetpackComposeDemoTheme {
+                ScrollingList()
+            }
+        }
+    }
+
+    @Composable
+    fun ScrollingList() {
+        val listSize = 100
+        // We save the scrolling position with this state
+        val scrollState = rememberLazyListState()
+        // We save the coroutine scope where our animated scroll will be executed
+        // This CoroutineScope will follow the lifecycle of the call site.
+        val coroutineScope = rememberCoroutineScope()
+
+        Column {
+            Row {
+                Button(onClick = {
+                    coroutineScope.launch {
+                        // 0 is the first item index
+                        scrollState.animateScrollToItem(0)
+                    }
+                }) {
+                    Text(text = "Scroll to Top")
+                }
+                Button(onClick = {
+                    coroutineScope.launch {
+                        // listSize - 1 is the last index of the list
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                }) {
+                    Text(text = "Scroll to End")
+                }
+            }
+
+            LazyColumn(state = scrollState) {
+                items(100) {
+                    ImageListItem(index = it)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun ImageListItem(index: Int) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = rememberImagePainter(
+                    data = "https://developer.android.com/images/brand/Android_Robot.png"
+                ),
+                contentDescription = "Android Logo",
+                modifier = Modifier.size(50.dp)
+            )
+            Spacer(Modifier.width(10.dp))
+            Text("Item #$index", style = MaterialTheme.typography.subtitle1)
         }
     }
 
@@ -80,6 +137,12 @@ class MainActivity : ComponentActivity() {
                     .padding(8.dp)
             )
         }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun ListPreview() {
+        ScrollingList()
     }
 
     @Composable
