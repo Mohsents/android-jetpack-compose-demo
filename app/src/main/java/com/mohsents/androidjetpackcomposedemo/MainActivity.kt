c31826c426ba1cec616559b3b9f62e2eed0dc9bf
Mohsents <mohsenteimury2000@gmail.com>
2021-12-25 16:35:52 +0330

    Add custom StaggeredGrid layout

--- app/src/main/java/com/mohsents/androidjetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/mohsents/androidjetpackcomposedemo/MainActivity.kt
@@ -3,9 +3,7 @@ package com.mohsents.androidjetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -23,6 +21,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
@@ -41,8 +40,121 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AndroidJetpackComposeDemoTheme {
-                ScrollingList()
+                BodyContent()
             }
+        }
+    }
+
+    @Composable
+    private fun StaggeredGrid(
+        modifier: Modifier = Modifier,
+        rows: Int = 3,
+        content: @Composable () -> Unit
+    ) {
+        Layout(
+            modifier = modifier,
+            content = content
+        )
+        { measurables, constraints ->
+            // measure our children. Remember you can only measure your children once.
+            // Keep track of the width of each row
+            val rowWidths = IntArray(rows) { 0 }
+
+            // Keep track of the max height of each row
+            val rowHeights = IntArray(rows) { 0 }
+
+            // Don't constrain child views further, measure them with given constraints
+            // List of measured children
+            val placeables = measurables.mapIndexed { index, measurable ->
+
+                // Measure each child
+                val placeable = measurable.measure(constraints)
+
+                // Track the width and max height of each row
+                val row = index % rows
+                rowWidths[row] += placeable.width
+                rowHeights[row] = rowHeights[row].coerceAtLeast(placeable.height)
+
+                placeable
+            }
+
+            // Grid's width is the widest row
+            val width = rowWidths.maxOrNull()
+                ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+                ?: constraints.minWidth
+
+            // Grid's height is the sum of the tallest element of each row
+            // coerced to the height constraints
+            val height = rowHeights.sumOf { it }
+                .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+            // Y of each row, based on the height accumulation of previous rows
+            val rowY = IntArray(rows) { 0 }
+            for (i in 1 until rows) {
+                rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+            }
+
+            // Set the size of the parent layout
+            layout(width, height) {
+                // x cord we have placed up to, per row
+                val rowX = IntArray(rows) { 0 }
+
+                placeables.forEachIndexed { index, placeable ->
+                    val row = index % rows
+                    placeable.placeRelative(
+                        x = rowX[row],
+                        y = rowY[row]
+                    )
+                    rowX[row] += placeable.width
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun Chip(modifier: Modifier = Modifier, text: String) {
+        Card(
+            modifier = modifier,
+            border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(16.dp, 16.dp)
+                        .background(color = MaterialTheme.colors.secondary)
+                )
+                Spacer(Modifier.width(4.dp))
+                Text(text = text)
+            }
+        }
+    }
+
+    private val topics = listOf(
+        "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+        "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+        "Religion", "Social sciences", "Technology", "TV", "Writing"
+    )
+
+    @Composable
+    private fun BodyContent(modifier: Modifier = Modifier) {
+        Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+            StaggeredGrid(modifier = modifier, 5) {
+                for (topic in topics) {
+                    Chip(modifier = Modifier.padding(8.dp), text = topic)
+                }
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun LayoutsCodelabPreview() {
+        AndroidJetpackComposeDemoTheme {
+            BodyContent()
         }
     }
 
@@ -117,7 +229,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun ScrollingList() {
+    private fun ScrollingList() {
         val listSize = 100
         // We save the scrolling position with this state
         val scrollState = rememberLazyListState()
@@ -154,7 +266,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun ImageListItem(index: Int) {
+    private fun ImageListItem(index: Int) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
                 painter = rememberImagePainter(
@@ -242,7 +354,7 @@ class MainActivity : ComponentActivity() {
      * This enables you to specify a modifier on a composable without having to name all parameters.
      */
     @Composable
-    fun PhotographerCard(modifier: Modifier = Modifier) {
+    private fun PhotographerCard(modifier: Modifier = Modifier) {
         Row(modifier = Modifier
             .padding(8.dp)
             .clip(RoundedCornerShape(4.dp))
@@ -273,7 +385,7 @@ class MainActivity : ComponentActivity() {
 
     @Preview(showBackground = true)
     @Composable
-    fun PhotographerCardPreview() {
+    private fun PhotographerCardPreview() {
         AndroidJetpackComposeDemoTheme {
             PhotographerCard()
         }
