626ea6127aeacaec6d940a42a9ac35486f0ddc0d
Mohsents <mohsenteimury2000@gmail.com>
2021-12-26 16:07:24 +0330

    Add simple examples of ConstraintLayout

--- app/src/main/java/com/mohsents/androidjetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/mohsents/androidjetpackcomposedemo/MainActivity.kt
@@ -30,6 +30,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.atLeast
 import coil.compose.rememberImagePainter
 import com.mohsents.androidjetpackcomposedemo.ui.theme.AndroidJetpackComposeDemoTheme
 import kotlinx.coroutines.launch
@@ -41,6 +44,56 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidJetpackComposeDemoTheme {
                 BodyContent()
+            }
+        }
+    }
+
+    @Composable
+    private fun LargeConstraintLayout() {
+        ConstraintLayout {
+            val text = createRef()
+
+            val guideline = createGuidelineFromStart(fraction = 0.5f)
+            Text(
+                "This is a very very very very very very very long text",
+                Modifier.constrainAs(text) {
+                    linkTo(start = guideline, end = parent.end)
+                    width = Dimension.preferredWrapContent.atLeast(100.dp)
+                }
+            )
+        }
+    }
+
+    @Composable
+    private fun ConstraintLayoutContent() {
+        ConstraintLayout {
+            // Creates references for the three composables
+            // in the ConstraintLayout's body
+            val (button1, button2, text) = createRefs()
+
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.constrainAs(button1) {
+                    top.linkTo(parent.top, margin = 16.dp)
+                }
+            ) {
+                Text("Button 1")
+            }
+
+            Text("Text", Modifier.constrainAs(text) {
+                top.linkTo(button1.bottom, margin = 16.dp)
+                centerAround(button1.end)
+            })
+
+            val barrier = createEndBarrier(button1, text)
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.constrainAs(button2) {
+                    top.linkTo(parent.top, margin = 16.dp)
+                    start.linkTo(barrier)
+                }
+            ) {
+                Text("Button 2")
             }
         }
     }
@@ -163,6 +216,14 @@ class MainActivity : ComponentActivity() {
     private fun LayoutsCodelabPreview() {
         AndroidJetpackComposeDemoTheme {
             BodyContent()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun LargeConstraintLayoutPreview() {
+        AndroidJetpackComposeDemoTheme {
+            LargeConstraintLayout()
         }
     }
 
@@ -331,6 +392,14 @@ class MainActivity : ComponentActivity() {
                     .padding(innerPadding)
                     .padding(8.dp)
             )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun ConstraintLayoutContentPreview() {
+        AndroidJetpackComposeDemoTheme {
+            ConstraintLayoutContent()
         }
     }
 
