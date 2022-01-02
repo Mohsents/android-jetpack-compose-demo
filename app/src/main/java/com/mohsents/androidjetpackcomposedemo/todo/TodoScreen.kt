869a4d16b520200b22472b0f85d163a847bc4500
Mohsents <mohsenteimury2000@gmail.com>
2022-01-02 19:38:49 +0330

    Add TodoItemInput composable

--- app/src/main/java/com/mohsents/androidjetpackcomposedemo/todo/TodoScreen.kt
+++ app/src/main/java/com/mohsents/androidjetpackcomposedemo/todo/TodoScreen.kt
@@ -25,7 +25,9 @@ import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,6 +49,11 @@ fun TodoScreen(
     onRemoveItem: (TodoItem) -> Unit
 ) {
     Column {
+        // add TodoItemInputBackground and TodoItem at the top of TodoScreen
+        TodoItemInputBackground(elevate = true, modifier = Modifier.fillMaxWidth()) {
+            TodoItemInput(onItemComplete = onAddItem)
+        }
+
         LazyColumn(
             modifier = Modifier.weight(1f),
             contentPadding = PaddingValues(top = 8.dp)
@@ -83,7 +90,7 @@ fun TodoScreen(
 fun TodoRow(
     todo: TodoItem,
     onItemClicked: (TodoItem) -> Unit,
-    modifier: Modifier   = Modifier,
+    modifier: Modifier = Modifier,
     iconAlpha: Float = remember(todo.id) { randomTint() }
 ) {
     Row(
@@ -112,6 +119,39 @@ fun TodoRow(
 private fun randomTint(): Float {
     return Random.nextFloat().coerceIn(0.3f, 0.9f)
 }
+
+@Composable
+private fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
+    val (text, setText) = remember { mutableStateOf("") }
+    Column {
+        Row(
+            Modifier
+                .padding(horizontal = 16.dp)
+                .padding(top = 16.dp)
+        ) {
+            TodoInputText(
+                text = text,
+                onTextChange = setText,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
+            )
+            TodoEditButton(
+                onClick = {
+                    onItemComplete(TodoItem(text)) // send onItemComplete event up
+                    setText("") // clear the internal text
+                },
+                text = "Add",
+                modifier = Modifier.align(Alignment.CenterVertically),
+                enabled = text.isNotBlank() // // enable if text is not blank
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTodoItemInput() = TodoItemInput(onItemComplete = {})
 
 @Preview
 @Composable
