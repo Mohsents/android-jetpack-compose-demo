b0b44bd214a85f3dcc3ade990b136454f48027fb
Mohsents <mohsenteimury2000@gmail.com>
2022-01-04 17:24:16 +0330

    Add AnimatedIconRow composable

--- app/src/main/java/com/mohsents/androidjetpackcomposedemo/todo/TodoScreen.kt
+++ app/src/main/java/com/mohsents/androidjetpackcomposedemo/todo/TodoScreen.kt
@@ -123,6 +123,13 @@ private fun randomTint(): Float {
 @Composable
 private fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember { mutableStateOf("") }
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default) }
+    val iconVisible = text.isNotBlank()
+    val submit = {
+        onItemComplete(TodoItem(text, icon)) // send onItemComplete event up
+        setIcon(TodoIcon.Default)
+        setText("") // clear the internal text
+    }
     Column {
         Row(
             Modifier
@@ -134,17 +141,24 @@ private fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
                 onTextChange = setText,
                 modifier = Modifier
                     .weight(1f)
-                    .padding(end = 8.dp)
+                    .padding(end = 8.dp),
+                onImeAction = submit
             )
             TodoEditButton(
-                onClick = {
-                    onItemComplete(TodoItem(text)) // send onItemComplete event up
-                    setText("") // clear the internal text
-                },
+                onClick = submit,
                 text = "Add",
                 modifier = Modifier.align(Alignment.CenterVertically),
                 enabled = text.isNotBlank() // // enable if text is not blank
             )
+        }
+        if (iconVisible) {
+            AnimatedIconRow(
+                icon = icon,
+                onIconChange = setIcon,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        } else {
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
