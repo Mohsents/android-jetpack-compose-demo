dcd95172ef665874f832497d3db94e6ad8c64a0d
Mohsents <mohsenteimury2000@gmail.com>
2022-01-15 18:31:43 +0330

    Setup the project

--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 The Android Open Source Project
+ * Copyright $YEAR The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,19 +14,3 @@
  * limitations under the License.
  */
 
-package com.mohsents.androidjetpackcomposedemo.ui.theme
-
-import androidx.compose.material.darkColors
-import androidx.compose.ui.graphics.Color
-
-val Green500 = Color(0xFF1EB980)
-val DarkBlue900 = Color(0xFF26282F)
-
-// Rally is always dark themed.
-val ColorPalette = darkColors(
-    primary = Green500,
-    surface = DarkBlue900,
-    onSurface = Color.White,
-    background = DarkBlue900,
-    onBackground = Color.White
-)
