32b26e37bfc783511853a107b4941e2344dd2fd2
Mohsents <mohsenteimury2000@gmail.com>
2022-01-18 19:07:43 +0330

    Adding test that current RallyTopAppBar's tab name's is exist or not

--- app/src/androidTest/java/com/mohsents/androidjetpackcomposedemo/TopAppBarTest.kt
+++ app/src/androidTest/java/com/mohsents/androidjetpackcomposedemo/TopAppBarTest.kt
@@ -3,6 +3,8 @@ package com.mohsents.androidjetpackcomposedemo
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.mohsents.androidjetpackcomposedemo.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -25,5 +27,22 @@ class TopAppBarTest {
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsDisplayed()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreen = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        // Log all nods for 'composeTestRule's content to debug.
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
     }
 }
