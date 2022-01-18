74fffc648895338be4d7bb69fa5a6fec473e57c7
Mohsents <mohsenteimury2000@gmail.com>
2022-01-18 18:40:59 +0330

    Add test that RallyTopAppBar's tab selection work or not

---
+++ app/src/androidTest/java/com/mohsents/androidjetpackcomposedemo/TopAppBarTest.kt
@@ -0,0 +1,29 @@
+package com.mohsents.androidjetpackcomposedemo
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import com.mohsents.androidjetpackcomposedemo.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreen = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsDisplayed()
+    }
+}
