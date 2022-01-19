84f3de8e093908612930fbb0e90a0ca017a19ba1
Mohsents <mohsenteimury2000@gmail.com>
2022-01-19 17:05:28 +0330

    Adding test that current selected RallyTopAppBar's tab label displayed or not

--- app/src/androidTest/java/com/mohsents/androidjetpackcomposedemo/TopAppBarTest.kt
+++ app/src/androidTest/java/com/mohsents/androidjetpackcomposedemo/TopAppBarTest.kt
@@ -1,10 +1,7 @@
 package com.mohsents.androidjetpackcomposedemo
 
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.printToLog
 import com.mohsents.androidjetpackcomposedemo.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -41,8 +38,13 @@ class TopAppBarTest {
         }
         // Log all nods for 'composeTestRule's content to debug.
         composeTestRule.onRoot().printToLog("currentLabelExists")
-        composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertExists()
+        composeTestRule.onNode(
+            hasText(RallyScreen.Accounts.name.uppercase()) and
+                    hasParent(
+                        hasContentDescription(
+                            RallyScreen.Accounts.name
+                        )
+                    ), useUnmergedTree = true
+        ).assertExists()
     }
 }
