5f26faf7a0632d858266b02b209e62be9a80e273
Mohsents <mohsenteimury2000@gmail.com>
2022-01-21 17:54:51 +0330

    Add description for navigate icon

--- app/src/main/java/com/mohsents/androidjetpackcomposedemo/ui/article/ArticleScreen.kt
+++ app/src/main/java/com/mohsents/androidjetpackcomposedemo/ui/article/ArticleScreen.kt
@@ -27,9 +27,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.insets.navigationBarsPadding
+import com.mohsents.androidjetpackcomposedemo.R
 import com.mohsents.androidjetpackcomposedemo.data.posts.PostsRepository
 import com.mohsents.androidjetpackcomposedemo.data.posts.post3
 import com.mohsents.androidjetpackcomposedemo.model.Post
@@ -90,7 +92,7 @@ fun ArticleScreen(
                     IconButton(onClick = onBack) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = null
+                            contentDescription = stringResource(id = R.string.cd_navigate_up)
                         )
                     }
                 }
