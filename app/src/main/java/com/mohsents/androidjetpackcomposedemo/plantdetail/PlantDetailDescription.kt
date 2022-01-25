a75ab015291902ed9616f1092e8bafff399f8967
Mohsents <mohsenteimury2000@gmail.com>
2022-01-25 16:47:08 +0330

    Add PlantName composable

--- app/src/main/java/com/mohsents/androidjetpackcomposedemo/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/mohsents/androidjetpackcomposedemo/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,41 @@
 
 package com.mohsents.androidjetpackcomposedemo.plantdetail
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.mohsents.androidjetpackcomposedemo.R
+
+@Composable
+fun PlantName(name: String) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth()
+    )
+}
 
 @Composable
 fun PlantDetailDescription() {
     Surface {
         Text("Hello Compose")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PlantNamePreview() {
+    MaterialTheme {
+        PlantName("Apple")
     }
 }
